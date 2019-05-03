212aea83b9678cce991309d64097284df7122de5
valeragh <valeragh@gmail.com>
2019-05-03 09:51:14 +0300

    edit costum admin email

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -5,25 +5,25 @@ class AdminMailer < ApplicationMailer
   def letter_admin_confirmation(letter)
     @letter = letter
 
-    mail to: "alinamagazin56@gmail.com", subject: "Новый вопрос от #{@letter.name}"
+    mail to: "nukleus.store@gmail.com", subject: "Новый вопрос от #{@letter.name}"
   end
 
   def order_admin_confirmation(order)
     @order = order
 
-    mail to: "alinamagazin56@gmail.com", subject: "Новый заказ №#{@order.id}"
+    mail to: "nukleus.store@gmail.com", subject: "Новый заказ №#{@order.id}"
   end
 
   def review_admin_confirmation(review)
     @review = review
 
-    mail to: "alinamagazin56@gmail.com", subject: "Новый отзыв от #{@review.name}"
+    mail to: "nukleus.store@gmail.com", subject: "Новый отзыв от #{@review.name}"
   end
 
   def back_phone_admin_confirmation(back_phone)
     @back_phone = back_phone
 
-    mail to: "alinamagazin56@gmail.com", subject: "Новый запрос на обратный звонок от #{@back_phone.name}"
+    mail to: "nukleus.store@gmail.com", subject: "Новый запрос на обратный звонок от #{@back_phone.name}"
   end
 
 end
